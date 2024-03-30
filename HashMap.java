a168b8737d923aa32067dac8ad00b7a90b2e4d5d
Kadim Arslanov <144515768+RyanGoslingIsNotDead@users.noreply.github.com>
2024-03-30 13:52:46 +0300

    Add files via upload

---
+++ HashMap.java
@@ -0,0 +1,124 @@
+public class HashMap<K, V> {
+    private Object[] array;
+    private int capacity;
+    private int size;
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+
+    public HashMap() {
+        this.capacity = DEFAULT_CAPACITY;
+        this.array = new Object[DEFAULT_CAPACITY];
+    }
+
+    public HashMap(int capacity) {
+        this.capacity = capacity;
+        this.array = new Object[capacity];
+    }
+    private int indexHash(int hash) {
+        return (capacity - 1) & hash;
+    }
+    private int hash(K key) {
+        return key.hashCode();
+    }
+
+    public void put(K key, V value) {
+        if ((float) size / capacity >= DEFAULT_LOAD_FACTOR){
+            resizing();
+        }
+        size++;
+        Node<K, V> currentNode = new Node<K, V>(key, value, null);
+        int index = indexHash(currentNode.getHash());
+        if (array[index] == null || getNodeByCell(array[index]).getKey().equals(key)) {
+            this.array[index] = currentNode;
+        } else {
+            Node<K, V> lastNode = getNodeByCell(array[index]);
+            while (lastNode.next != null){
+                lastNode = lastNode.next;
+            }
+            lastNode.next = currentNode;
+        }
+    }
+    public Node<K, V> getNode(K key) {
+        if (containsKey(key)) {
+            return (Node<K, V>) array[indexHash(hash(key))];
+        } else {
+            throw new IllegalArgumentException("Wrong key");
+        }
+    }
+    private Node<K, V> getNodeByCell(Object cell) {
+        return (Node<K, V>) cell;
+    }
+
+    public V get(K key) {
+        if (containsKey(key)) {
+            return getNode(key).getValue();
+        } else {
+            throw new IllegalArgumentException("Wrong key");
+        }
+    }
+
+    public boolean containsKey(K key) {
+        int index = indexHash(hash(key));
+        Node<K, V> currentNode = getNodeByCell(array[index]);
+        while (currentNode.next != null);{
+            if (currentNode.getKey().equals(key)){
+                return true;
+            }
+            currentNode = currentNode.next;
+        }while (currentNode.next != null);
+        return false;
+    }
+
+    public boolean containsValue(V value) {
+        for (Object element : array) {
+            if (element instanceof Node) {
+                Node<K, V> currentNode = (Node<K, V>) element;
+                do {
+                    if (currentNode.getValue().equals(value)) {
+                        return true;
+                    }
+                    currentNode = currentNode.next;
+                } while (currentNode.next != null);
+            }
+        }
+        return false;
+    }
+    public void remove(K key){
+        if (containsKey(key)) {
+            size--;
+            Node<K, V> currentNode = getNode(key);
+            while (currentNode.next != null) {
+                if (currentNode.next.getKey().equals(key)) {
+                    currentNode.next = currentNode.next.next;
+                    break;
+                }
+                currentNode = currentNode.next;
+            }
+            currentNode = null;
+        } else {
+            throw new IllegalArgumentException("Wrong key");
+        }
+    }
+    public int size(){
+        return size;
+    }
+    public void clear(){
+        for(int i =0; i<size;i++){
+            array[i]=null;
+        }
+        size = 0;
+    }
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    private void resizing(){
+        capacity = capacity * 2;
+        Object[] newArray = new Object[capacity];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        array = newArray;
+    }
+    public int getCapacity(){
+        return capacity;
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("KINGONE", 100);
+        map.put("KINGTWO", 200);
+        map.remove("KINGOINE");
+        System.out.println(map.size());
+    }
+}
